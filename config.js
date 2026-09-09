be9f3284da23a5ab2830e03511dd33ae6cb32102
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-09 14:13:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x8cC2Efec68548ef04D515f2f6C35CA5105A813F2",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/memematter_rh",
   NAME:    "Meme Matter",
   TICKER:  "$MATTER",
